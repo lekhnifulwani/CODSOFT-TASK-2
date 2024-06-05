6633e380ce9774cd5d9c90b51f6a3592a3397534
Lekhni Fulwani <lekhnifulwani1@gmail.com>
2024-06-05 21:57:48 +0530

    Add files via upload

---
+++ Task- 2/main.js
@@ -0,0 +1,9 @@
+function msg()
+{
+  window.location.href = "https://github.com/lekhnifulwani/musicplayer-page.git/";
+
+}
+function msg1()
+{
+window.location.href = "https://github.com/lekhnifulwani/CODSOFT-TASK.git/";
+}
